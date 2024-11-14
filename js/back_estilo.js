e3e05d5100c25ccdac6594ee1a622d546ec62aca
Biel2.0 <125614460+Bobiel@users.noreply.github.com>
2024-11-14 08:57:42 -0300

    Delete js directory

--- js/back_estilo.js
+++
@@ -1,9 +0,0 @@
-//var im_1 = window.document.getElementById("gato")
-
-//function AM_im_1(){ 
- //   im_1.style.borderBottom="120px solid black"
-//    im_1.style.bor
-//    im_1.style.transition="0.55s"
-//    im_1.style.color="white"
-//    im_1.innerText="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-//}
