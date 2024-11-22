d0bbaf070941ed91e455a00fbf96b0ae6f3f8472
Biel2.0 <125614460+Bobiel@users.noreply.github.com>
2024-11-22 10:36:50 -0300

    Add files via upload

---
+++ js/encomendas.js
@@ -0,0 +1,5 @@
+ScrollReveal({reset:true}).reveal('.product-section',{
+    origin: 'left',
+    duration: 2000,
+    distance:'20%'
+})
---
+++ js/galeria.js
@@ -0,0 +1,6 @@
+ScrollReveal({reset:true}).reveal('.imagens',{
+    origin: 'bottom',
+    duration: 2000,
+    delay: 100,
+    distance:'20%'
+})
